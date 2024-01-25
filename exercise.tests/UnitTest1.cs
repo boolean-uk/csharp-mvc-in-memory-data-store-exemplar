5e8a2eb9c4e497266beef36a59067c90a7e0af32
Nigel <nigel.sibbert@gmail.com>
2024-01-25 17:20:36 +0000

    another test

--- exercise.tests/UnitTest1.cs
+++ exercise.tests/UnitTest1.cs
@@ -28,13 +28,32 @@ public class Tests
         var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
         var client = factory.CreateClient();
 
-        // Act
         ProductPost model = new ProductPost() { name = "Banana", category = "Fruit", price = 100 };
         string json = JsonConvert.SerializeObject(model);
         StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+        
+        // Act
         var response = await client.PostAsync("/products", httpContent);
 
         // Assert
-        Assert.IsTrue(response.StatusCode == System.Net.HttpStatusCode.Created);
+        Assert.That(response.StatusCode==(System.Net.HttpStatusCode.Created));
+    }
+    [Test]
+    public async Task AddDuplicateProductTest()
+    {
+        // Arrange
+        var factory = new WebApplicationFactory<Program>().WithWebHostBuilder(builder => { });
+        var client = factory.CreateClient();
+
+        ProductPost model = new ProductPost() { name = "Banana", category = "Fruit", price = 100 };
+        string json = JsonConvert.SerializeObject(model);
+        StringContent httpContent = new StringContent(json, System.Text.Encoding.UTF8, "application/json");
+
+        // Act
+        var responseFirst = await client.PostAsync("/products", httpContent);
+        var responseSecond = await client.PostAsync("/products", httpContent);
+
+        // Assert
+        Assert.That(responseSecond.StatusCode == (System.Net.HttpStatusCode.BadRequest));
     }
 }
--- exercise.wwwapi/Program.cs
+++ exercise.wwwapi/Program.cs
@@ -35,4 +35,4 @@ app.UseHttpsRedirection();
 app.ConfigureProductEndpoint();
 app.Run();
 
-public partial class Program { }
+public partial class Program { } //required for the testing project!
